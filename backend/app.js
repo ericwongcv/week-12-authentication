e482f16672a96cfa50fb1e6287909711c5c3a6a5
Eric Wong <contact.eric.wong@gmail.com>
2022-07-19 17:56:14 -0700

    update Authenticate Me Phase 1 - error handlers

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const { ValidationError } = require('sequelize');
+
 // check if current environment is production
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -51,5 +53,36 @@ if (!isProduction) {
 const routes = require('./routes');
 // connect all routes
 app.use(routes);
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
